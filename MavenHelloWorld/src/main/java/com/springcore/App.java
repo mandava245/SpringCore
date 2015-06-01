052434169a154366fc8da49c1f3c31e86d657039
Pavan Mandava <mspavan04@gmail.com>
2015-06-01 16:14:23 +0530

    Initial Commit of MavenRepositoryFirstApplication on 1st June 2015

---
+++ MavenHelloWorld/src/main/java/com/springcore/App.java
@@ -0,0 +1,10 @@
+package com.springcore;
+
+public class App
+{
+    public static void main( String[] args)
+    {
+        System.out.println( "Hello World !" );
+        System.out.println("Welcome to Maven First Project...!!!");
+    }
+}
